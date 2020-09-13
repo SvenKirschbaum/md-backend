1aa08624a6b7e7ee4b6bc02a125c86d1f1e63131
Sven Kirschbaum <sven@elite12.de>
2020-09-13 17:44:07 +0200

    Initial Commit

---
+++ src/main/java/de/markusdope/stats/MarkusDopeStatsApplication.java
@@ -0,0 +1,13 @@
+package de.markusdope.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkusDopeStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkusDopeStatsApplication.class, args);
+	}
+
+}
