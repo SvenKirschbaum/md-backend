c2baff1f984cf6a7f5915d1da2e6c28223d05177
Sven Kirschbaum <sven@kirschbaum.me>
2021-08-25 07:50:56 +0200

    :coffin: Remove error suppression

--- src/main/java/de/markusdope/stats/MarkusDopeStatsApplication.java
+++ src/main/java/de/markusdope/stats/MarkusDopeStatsApplication.java
@@ -7,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarkusDopeStatsApplication {
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(MarkusDopeStatsApplication.class, args);
-		}
-		catch (Exception e) {
-			//Shutdown JVM on Spring Context initialization Errror
-			System.exit(-1);
-		}
+		SpringApplication.run(MarkusDopeStatsApplication.class, args);
 	}
 
 }
