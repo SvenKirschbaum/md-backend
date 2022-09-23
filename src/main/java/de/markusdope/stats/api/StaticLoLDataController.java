c38534b6cc1d848b2cd25a73d9f1a98c4ed131e9
Sven Kirschbaum <sven@kirschbaum.me>
2022-09-23 18:10:14 +0200

    :bug: Return 404 if requested images do not exist

--- src/main/java/de/markusdope/stats/api/StaticLoLDataController.java
+++ src/main/java/de/markusdope/stats/api/StaticLoLDataController.java
@@ -1,7 +1,9 @@
 package de.markusdope.stats.api;
 
 import com.merakianalytics.orianna.Orianna;
+import de.markusdope.stats.exception.NotFoundException;
 import de.markusdope.stats.service.VersionService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,6 @@ import reactor.core.scheduler.Schedulers;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/static/{version}")
@@ -25,45 +26,40 @@ public class StaticLoLDataController {
 
     @GetMapping(value = "/champion/image/{championid}", produces = MediaType.IMAGE_PNG_VALUE)
     public Mono<byte[]> getChampion(@PathVariable int championid, @PathVariable String version) {
-        return this.imageToByteArray(
-                Mono.just(championid)
-                        .publishOn(Schedulers.boundedElastic())
-                        .map(id -> Orianna.championWithId(id).withVersion(versionService.fromMatch(version)).get())
-                        .map(champion -> champion.getImage().get())
-        );
+        return Mono.just(championid)
+            .publishOn(Schedulers.boundedElastic())
+            .map(id -> Orianna.championWithId(id).withVersion(versionService.fromMatch(version)).get())
+            .flatMap(champion -> champion.exists() ? Mono.just(champion) : Mono.error(new NotFoundException()))
+            .map(champion -> champion.getImage().get())
+            .map(this::imageToByteArray);
     }
 
     @GetMapping(value = "/summonerSpell/image/{spellId}", produces = MediaType.IMAGE_PNG_VALUE)
     public Mono<byte[]> getSummonerSpell(@PathVariable int spellId, @PathVariable String version) {
-        return this.imageToByteArray(
-                Mono.just(spellId)
-                        .publishOn(Schedulers.boundedElastic())
-                        .map(id -> Orianna.summonerSpellWithId(id).withVersion(versionService.fromMatch(version)).get().getImage().get())
-        );
+        return Mono.just(spellId)
+            .publishOn(Schedulers.boundedElastic())
+            .map(id -> Orianna.summonerSpellWithId(id).withVersion(versionService.fromMatch(version)).get())
+            .flatMap(spell -> spell.exists() ? Mono.just(spell) : Mono.error(new NotFoundException()))
+            .map(spell -> spell.getImage().get())
+            .map(this::imageToByteArray);
     }
 
     @GetMapping(value = "/item/image/{itemId}", produces = MediaType.IMAGE_PNG_VALUE)
     public Mono<byte[]> getItem(@PathVariable int itemId, @PathVariable String version) {
-        return this.imageToByteArray(
-                Mono.just(itemId)
-                        .publishOn(Schedulers.boundedElastic())
-                        .map(id -> Orianna.itemWithId(id).withVersion(versionService.fromMatch(version)).get().getImage().get())
-        );
+        return Mono.just(itemId)
+            .publishOn(Schedulers.boundedElastic())
+            .map(id -> Orianna.itemWithId(id).withVersion(versionService.fromMatch(version)).get())
+            .flatMap(item -> item.exists() ? Mono.just(item) : Mono.error(new NotFoundException()))
+            .map(item -> item.getImage().get())
+            .map(this::imageToByteArray);
     }
 
 
-    private Mono<byte[]> imageToByteArray(Mono<BufferedImage> imageMono) {
-        return imageMono
-                .flatMap(bufferedImage -> {
-                    try {
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                        //While ImageIO.write can do IO operations, our usage operates only in memory, so doing this in an reactive context is ok
-                        //noinspection BlockingMethodInNonBlockingContext
-                        ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
-                        return Mono.just(byteArrayOutputStream.toByteArray());
-                    } catch (IOException e) {
-                        return Mono.error(e);
-                    }
-                });
+    //Our usage of ImageIO.write operates only in memory, so the IO Exception should not be able to occur
+    @SneakyThrows
+    private byte[] imageToByteArray(BufferedImage image) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
 }
